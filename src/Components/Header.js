22d8763defe9e91fd18a9f607d3496d9c7b7ac1d
JaeyeonKIM <foodsksms@gmail.com>
2020-06-13 14:44:40 +0900

    Make Header CSS

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,53 @@
 import React from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import styled from 'styled-components';
+const Header = styled.header`
+    color:white;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 0px 10px;
+    background-color: rbga(20, 20, 20, 0.8);
+    z-index: 10;
+    box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 
-export default () => (
-    <header>
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/upcoming">Upcoming</a></li>
-            <li><a href="/watchlist">Watch List</a></li>
-        </ul>
-    </header>
-);
+// eslint-disable-next-line no-undef
+const List = styled.ul`
+    display:flex;
+`;
+
+const Item = styled.li`
+    width: 80px;
+    height: 50px
+    text-align: center;
+    border-bottom: 5px solid ${props => (props.current ? "#3498db" : "transparent")};
+    transition: border-bottom 0.5s ease-in-out;
+`;
+
+const SLink = styled(Link)`
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export default withRouter(({location: {pathname}}) => (
+    <Header>
+        <List>
+            <Item current={pathname === "/"}>
+                <SLink to="/">Home</SLink>
+            </Item>
+            <Item current={pathname === "/upcoming"}>
+                <SLink to="/upcoming">Upcoming</SLink>
+            </Item>
+            <Item current={pathname === "/watchlist"}>
+                <SLink to="/watchlist">Watch List</SLink>
+            </Item>
+        </List>
+    </Header>
+));
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,14 +3,19 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 import Home from 'Routes/Home';
 import Upcoming from 'Routes/Upcoming';
 import WatchList from 'Routes/WatchList';
+import Header from './Header';
 
 export default () => (
     <Router>
+        <>
+        <Header />
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/upcoming" component={Upcoming} />
             <Route path="/watchlist" component={WatchList} />
             <Redirect from="*" to="/" />
         </Switch>
+        </>
     </Router>
 )
+
