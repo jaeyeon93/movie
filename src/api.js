9365172fa9c3d09dcc60bf339b82741a76b39cfa
JaeyeonKIM <foodsksms@gmail.com>
2020-06-13 15:16:43 +0900

    Request upcoming movie

--- src/api.js
+++ src/api.js
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.themoviedb.org/3/',
-    params: {
-        api_key: 'hh',
-        language: 'en-US',
+    baseURL: `https://api.themoviedb.org/3/`
+});
 
-    }
-})
+const upcomingParams = {
+    api_key: `25c489c850818fb8c91a684fa9000603`,
+    language: `en-US`
+};
+
+const upcommingMovie = async () => await api.get(`movie/upcoming`, {params: upcomingParams});
+upcommingMovie();
+
+export default api;
