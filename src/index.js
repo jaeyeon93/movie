9cbb6fd51fe94c951a05bb78cfdc49a157831492
JaeyeonKIM <foodsksms@gmail.com>
2020-06-15 00:20:46 +0900

    [issue-4] 리덕스 설정중
    - reducer, action, store 생성 및 정의

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 import './api';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+import wishList from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(wishList, devTools);
+console.log(`store : ${JSON.stringify(store)}`);
+console.log(`devTool : ${devTools}`);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
